988d4eece723a94341fda2edd6d4fb99a11405ac
cheekong <cheekong.t@gmail.com>
2018-11-09 01:45:36 +1100

    Modal Abstraction for Basic Modal

--- src/components/Modal/BasicModal/BasicModal.js
+++ src/components/Modal/BasicModal/BasicModal.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Button from '../UI/Button/Button';
+import Button from '../../UI/Button/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './BasicModal.css';
 
@@ -29,18 +29,9 @@ class Modal extends Component {
     }
 
     render(){
-        let options = [];
-        if(this.props.options.length){
-            options = this.props.options.map((optionItem, idx) => {
-                return(
-                    <option key={optionItem.name + idx} value={optionItem.name}>#{optionItem.label}</option>
-                )
-            })
-        }
-
         let modalClassName = ['basic-modal'];
         
-        if(this.state.reserve){
+        if(this.state.reserve || this.props.reserve){
             modalClassName.push('reserve');
         }
         
@@ -54,38 +45,13 @@ class Modal extends Component {
                     <div className='basic-modal__container '>
                         <div className='basic-modal__header'>
                             <h2 className='basic-modal__heading'>{this.props.title}</h2>
-                            <p className='basic-modal__cancel' onClick={() => this.reserveModal()}><FontAwesomeIcon icon="times" /></p>
+                            <p className='basic-modal__cancel' 
+                                onClick={() => this.reserveModal()}>
+                                <FontAwesomeIcon icon="times" />
+                            </p>
                         </div>
                         <div className='basic-modal__body'>
-                            <div className='share-vid-modal__description'>
-                                <h3>{this.props.subtitle}</h3>
-                                <p >
-                                    To Share this clip, add email addresses separated by commas, then click 'Send'.
-                                </p>
-                                <p>
-                                    <span className='video-clip-description'>Projects0001 Report / Task 2 / </span> 
-                                    <span className='participant-description'>Partcipants 4</span>
-                                    <br />
-                                    
-                                    <span className='font-awesome-icon'><FontAwesomeIcon icon="video" /></span>
-                                    <span className='participant-description'>Start:</span> 
-                                    <span className='video-clip-description'>2:30    </span>
-                                    <span className='participant-description'>End</span> 
-                                    <span className='video-clip-description'> 2:30</span>
-                                </p>
-                            </div>
-                            <div className='share-vid-modal__cta'>
-                                <select className='select'
-                                    onChange={(event) => this.props.onChange(event.target.value)}>
-                                    <option value='select'>Select</option>
-                                    {options}
-                                </select>
-                                <Button 
-                                    modifier='modal'
-                                    label='Share with Slack'
-                                    disabled={this.props.disabled}
-                                    onClick={() => this.handleSubmit()}/>
-                            </div>
+                            {this.props.children}
                         </div>
                     </div>
                     
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Button from '../UI/Button/Button';
+import BasicModal from './BasicModal/BasicModal';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './Modal.css';
 
@@ -32,66 +33,50 @@ class Modal extends Component {
         let options = [];
         if(this.props.options.length){
             options = this.props.options.map((optionItem, idx) => {
+console.log('optionItem',optionItem);
                 return(
                     <option key={optionItem.name + idx} value={optionItem.name}>#{optionItem.label}</option>
                 )
             })
         }
 
-        let modalClassName = ['share-vid-modal'];
-        
-        if(this.state.reserve){
-            modalClassName.push('reserve');
-        }
-        
-        if(!this.props.show){
-            modalClassName.push('inactive');
-        }
-        modalClassName = modalClassName.join(' ');
         return (
-            <div className={modalClassName}>
-                <div className='share-vid-modal__window'>
-                    <div className='share-vid-modal__container '>
-                        <div className='share-vid-modal__header'>
-                            <h2 className='share-vid-modal__heading'>{this.props.title}</h2>
-                            <p className='share-vid-modal__cancel' onClick={() => this.reserveModal()}><FontAwesomeIcon icon="times" /></p>
-                        </div>
-                        <div className='share-vid-modal__body'>
-                            <div className='share-vid-modal__description'>
-                                <h3>{this.props.subtitle}</h3>
-                                <p >
-                                    To Share this clip, add email addresses separated by commas, then click 'Send'.
-                                </p>
-                                <p>
-                                    <span className='video-clip-description'>Projects0001 Report / Task 2 / </span> 
-                                    <span className='participant-description'>Partcipants 4</span>
-                                    <br />
-                                    
-                                    <span className='font-awesome-icon'><FontAwesomeIcon icon="video" /></span>
-                                    <span className='participant-description'>Start:</span> 
-                                    <span className='video-clip-description'>2:30    </span>
-                                    <span className='participant-description'>End</span> 
-                                    <span className='video-clip-description'> 2:30</span>
-                                </p>
-                            </div>
-                            <div className='share-vid-modal__cta'>
-                                <select className='select'
-                                    onChange={(event) => this.props.onChange(event.target.value)}>
-                                    <option value='select'>Select</option>
-                                    {options}
-                                </select>
-                                <Button 
-                                    modifier='modal'
-                                    label='Share with Slack'
-                                    disabled={this.props.disabled}
-                                    onClick={() => this.handleSubmit()}/>
-                            </div>
-                        </div>
-                    </div>
-                    
+            <BasicModal
+                show={this.props.show}
+                title={this.props.title}
+                onClick={this.props.onClick}
+                onSubmit={() => this.handleSubmit()}
+                reverse={this.state.reserve}>
+                <div className='share-vid-modal__description'>
+                    <h3>{this.props.subtitle}</h3>
+                    <p >To Share this clip, add email addresses separated by commas, then click 'Send'.</p>
+                    <p>
+                        <span className='video-clip-description'>Projects0001 Report / Task 2 / </span> 
+                        <span className='participant-description'>Partcipants 4</span>
+                        <br />
+                        
+                        <span className='font-awesome-icon'><FontAwesomeIcon icon="video" /></span>
+                        <span className='participant-description'>Start:</span> 
+                        <span className='video-clip-description'>2:30    </span>
+                        <span className='participant-description'>End</span> 
+                        <span className='video-clip-description'> 2:30</span>
+                    </p>
                 </div>
-            </div>
+                <div className='share-vid-modal__cta'>
+                    <select className='select'
+                        onChange={(event) => this.props.onChange(event.target.value)}>
+                        <option value='select'>Select</option>
+                        {options}
+                    </select>
+                    <Button 
+                        modifier='modal'
+                        label='Share with Slack'
+                        disabled={this.props.disabled}
+                        onClick={() => this.handleSubmit()}/>
+                </div>
+            </BasicModal>
         );
+       
     }
 }
 
